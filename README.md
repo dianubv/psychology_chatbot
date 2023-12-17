03abdea49d90bb685a4821a1b74b89377f197cbf
dianubv <diane.bouffardvercelli@gmail.com>
2023-12-18 00:33:48 +0100

    add style for website

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
+## Overview
 The project is a GPT like which give you psychology advices if you suffer of anxiety or other psychological disorders.
 
 It's based on the project from nanoGPT project from Karparthy (https://github.com/karpathy/nanoGPT)
 
-The web part of the project doesn't work
+The web part of the project doesn't work.
 
+## Starting 
+You need to download some pip package :
+``$ pip install -r requirements.txt``
 
-## To train : 
+## Training 
+##### Prepare the dataset :
+For the basic one :
+``python data/psychology_char/prepare.py``
+
+For the basic one but based on gpt2 : 
+``python data/psychology_char/prepare.py``
+
+For the evil version : 
+``python data/psychology_char/prepare.py``
+
+##### Start the training :
 For the basic one :
 ``$ python train.py config/train_psychology.py``
 
 For the basic one but based on gpt2 : 
 ``$ python train.py config/finetune_psychology.py``
 
-For the evil advice chat : 
+For the evil version : 
 ``$ python train.py config/train_not_nice.py``
 
-## To test :
+## Testing
 ##### In the terminal :
-``$ python3.10 sample.py --out_dir=out-psychology-char``
+``$ python3.10 web.py --out_dir=out-psychology-char``
 
 You can replace the out_dir by the others 
 
---
+++ web.py
@@ -0,0 +1,96 @@
+"""
+Sample from a trained model for the web. It have more errors tha sample.py but it create an .onnx file.
+"""
+import torch._dynamo
+torch._dynamo.config.suppress_errors = True
+
+import os
+import pickle
+from contextlib import nullcontext
+import torch
+import tiktoken
+from model import GPTConfig, GPT
+
+# -----------------------------------------------------------------------------
+init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+out_dir = 'out-psychology-char' # ignored if init_from is not 'resume'
+start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 10 # number of samples to draw
+max_new_tokens = 500 # number of tokens generated in each sample
+temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+seed = 1337
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
+compile = True # use PyTorch 2.0 to compile the model to be faster
+exec(open('configurator.py').read()) # overrides from command line or config file
+# -----------------------------------------------------------------------------
+
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+print(out_dir)
+# init from a model saved in a specific directory
+ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+print(f"Loading checkpoint from {ckpt_path}...")
+checkpoint = torch.load(ckpt_path, map_location=device)
+gptconf = GPTConfig(**checkpoint['model_args'])
+model = GPT(gptconf)
+state_dict = checkpoint['model']
+unwanted_prefix = '_orig_mod.'
+for k,v in list(state_dict.items()):
+    if k.startswith(unwanted_prefix):
+        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+model.load_state_dict(state_dict)
+print(f"Loaded checkpoint {model}!")
+
+model.eval()
+model.to(device)
+model = torch.compile(model) # requires PyTorch 2.0 (optional)
+
+# look for the meta pickle in case it is available in the dataset folder
+load_meta = False
+if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
+    meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
+    load_meta = os.path.exists(meta_path)
+    print(f"looking for meta.pkl in dataset folder: {load_meta}, {meta_path}")
+if load_meta:
+    print(f"Loading meta from {meta_path}...")
+    with open(meta_path, 'rb') as f:
+        meta = pickle.load(f)
+    # TODO want to make this more general to arbitrary encoder/decoder schemes
+    stoi, itos = meta['stoi'], meta['itos']
+    print(f"Loaded meta: {len(stoi):,} tokens")
+    print(f"meta: {meta['itos'].values()}") 
+    # print(f"itos: {itos}")
+    encode = lambda s: [stoi[c] for c in s]
+    decode = lambda l: ''.join([itos[i] for i in l])
+else:
+    # ok let's assume gpt-2 encodings by default
+    print("No meta.pkl found, assuming GPT-2 encodings...")
+    enc = tiktoken.get_encoding("gpt2")
+    encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
+    decode = lambda l: enc.decode(l)
+
+
+model.eval()
+user_input = input("What is your problem? ")
+
+context = torch.tensor([encode(user_input)], dtype=torch.int32, device='cuda')
+print(decode(model.generate(context, max_new_tokens=100)[0].tolist()))
+
+dummy_input = torch.zeros(1, 64, dtype=torch.int32).to("cuda")
+
+torch.onnx.export(model,
+                dummy_input,
+                "model.onnx",
+                input_names=['inputs'],
+                output_names=['outputs'], 
+                verbose=False,
+                opset_version=17,
+                dynamic_axes={'inputs': {0: 'batch_size', 1: 'sequence_length'}, 'outputs': {0: 'batch_size', 1: 'sequence_length'}}
+)
